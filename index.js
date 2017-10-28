a4b0e330f7a3aca267835a3116f3a2d7aac53146
Ben (OutdatedVersion) <outdated@outdatedversion.com>
2017-10-28 16:51:06 -0500

    .

--- index.js
+++ index.js
@@ -21,3 +21,26 @@ Inquirer.prompt(questions).then(answers => {
         solve(parseInt(answers.add), parseInt(answers.product))
     )
 })
+
+
+/**
+ * Take in the two numbers and factor out the required solutions for it. In the event
+ * that there is no actual solution, text will be returned stating so.
+ * 
+ * @param {Number} add The number that the two numbers must add up to
+ * @param {Number} product The number that the two numbers must multiply to
+ * @returns {String} The answer or text stating we couldn't find an answer
+ */
+const solve = (add, product) => 
+{
+    const insideRoot = add * add - 4 * product
+
+    if (insideRoot >= 0)
+    {
+        const b = Math.round((add + Math.sqrt(insideRoot)) / 2)
+        const a = Math.round(add - b)
+
+        return `Use ${a} and ${b}`
+    }
+    else return 'No real solution'
+}
